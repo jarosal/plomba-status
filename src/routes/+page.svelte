53a4eff9b0f89043d7d137fe0891f897fd924870
KacperNiwczyk <kacper.niwczyk@relativity.com>
2023-07-21 17:48:55 +0200

    second

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 
 <div class="flex flex-col place-content-center min-h-screen">
     <div class="flex flex-col items-center justify-items-center text-5xl m-2 p-2 gap-2">
-        <span class="gradient-heading">Czy opłaca się zerwać plombe na funfit?</span>
+        <span class="gradient-heading">Czy opłaca się zerwać plombe na funfit? </span>
         <p></p>
         <p class:text-success-500={isProfitable} class:text-error-500={!isProfitable}>{isProfitable ? "TAK" : "NIE"}</p>
         <p>Aktualna cena to {price} zł</p>
