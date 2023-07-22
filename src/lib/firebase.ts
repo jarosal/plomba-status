5834cf3e4a09dbec97772ae5aca9fd7bbbe49226
KacperNiwczyk <kacperniwczyk@gmail.com>
2023-07-22 16:20:10 +0200

    Added firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyM6IWSYMwAeD7frMjfQhry2i4JztcdZ8",
+  authDomain: "polmba-b3479.firebaseapp.com",
+  projectId: "polmba-b3479",
+  storageBucket: "polmba-b3479.appspot.com",
+  messagingSenderId: "990036766933",
+  appId: "1:990036766933:web:e6312cf3fd4a4cf4dd725c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
