7638457015fd631f85216f5c07adeca796af95f9
KacperNiwczyk <kacperniwczyk@gmail.com>
2023-07-22 12:35:01 +0200

    Added prettier and animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,71 @@
 <script lang="ts">
-    import { fly } from "svelte/transition"
+  import { backOut } from "svelte/easing";
+  import { fly } from "svelte/transition";
+  import { onMount } from "svelte";
 
-    let price: number = 50;
-    $: isProfitable = price <= 25
+  let price: number = 50;
+  let show = false;
 
-    let words = ['Czy opłaca się', 'zerwać plombe', 'na funfit>']
+  onMount(() => (show = true));
+
+  $: isProfitable = price <= 25;
+
+  let decision: string = isProfitable ? "TAK" : "NIE";
+
+  const words = ["Czy opłaca sie", "", "zerwać plombe", "", "na funfit?"];
 </script>
 
 <div class="flex flex-col place-content-center min-h-screen">
-    <div class="flex flex-col items-center justify-items-center text-5xl m-2 p-2 gap-2">
-        <span class="gradient-heading">Czy opłaca się zerwać plombe na funfit?</span>
-        <p></p>
-        <p class:text-success-500={isProfitable} class:text-error-500={!isProfitable}>{isProfitable ? "TAK" : "NIE"}</p>
-        <p>Aktualna cena to {price} zł</p>
+  <div
+    class="flex flex-col items-center justify-items-center text-5xl m-2 p-2 gap-2"
+  >
+    <div class="flex flex-col card m-4 p-4 pb-6">
+      <div class="inline-block overflow-hidden max-w-[900px] text-center">
+        {#each words as word, i}
+          {#if show}
+            <span
+              in:fly={{
+                y: 100,
+                delay: 300 * i,
+                easing: backOut,
+              }}
+              class="inline-block m-1"
+            >
+              {word}
+            </span>
+          {/if}
+        {/each}
+      </div>
+      {#if show}
+        <div class="m-2 pt-4 text-center tracking-wide">
+          <p
+            in:fly={{
+              y: 100,
+              delay: 300 * (words.length + 1),
+              easing: backOut,
+            }}
+            class="font-bold"
+            class:text-success-500={isProfitable}
+            class:text-error-500={!isProfitable}
+          >
+            {decision}
+          </p>
+        </div>
+      {/if}
+      {#if show}
+        <div class="inline-block text-center">
+          <p
+            in:fly={{
+              y: 50,
+              delay: 300 * (words.length + 2),
+              easing: backOut,
+            }}
+            class="text-strong"
+          >
+            Aktualna cena to {price} zł
+          </p>
+        </div>
+      {/if}
     </div>
+  </div>
 </div>
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: ['./src/**/*.{html,js,svelte,ts}', require('path').join(require.resolve(
-    '@skeletonlabs/skeleton'),
-    '../**/*.{html,js,svelte,ts}')],
+  darkMode: "class",
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    require("path").join(
+      require.resolve("@skeletonlabs/skeleton"),
+      "../**/*.{html,js,svelte,ts}",
+    ),
+  ],
   theme: {
     extend: {},
   },
-  plugins: [...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()],
-}
-
+  plugins: [...require("@skeletonlabs/skeleton/tailwind/skeleton.cjs")()],
+};
